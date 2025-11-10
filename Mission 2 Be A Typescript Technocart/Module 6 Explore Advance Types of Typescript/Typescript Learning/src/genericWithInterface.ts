df17c436587cb68d531c446dfadffbba31a776e6
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-11-10 21:37:03 +0600

    Finished watching all 5 videos of Module 6

---
+++ Mission 2 Be A Typescript Technocart/Module 6 Explore Advance Types of Typescript/Typescript Learning/src/genericWithInterface.ts
@@ -0,0 +1,62 @@
+
+
+interface Developer<T, X = null> {
+    name: string;
+    salary: number;
+    device: {
+        brand: string;
+        model: string;
+        releasedYear: string;
+    };
+
+    smartWatch: T;
+    bike?: X;
+};
+
+
+interface BrandCharaWatch {
+    heartRate: string;
+    stopWatch: boolean;
+}
+
+interface AppleWatch {
+    heartRate: string;
+    callSupport: boolean;
+    calculator: boolean;
+    AiFeature: boolean;
+}
+
+
+const poorDeveloper: Developer<BrandCharaWatch, {
+    brand: 'Yamaha', engineCapacity: '200cc'
+}> = {
+    name: 'Mr. Poor',
+    salary: 20,
+    device: {
+        model: 'lenovo',
+        brand: 'A21',
+        releasedYear: '2010'
+    },
+    smartWatch: {
+        heartRate: '200',
+        stopWatch: true
+    },
+};
+
+const richDeveloper: Developer<AppleWatch> = {
+    name: 'Mr. Y',
+    salary: 100,
+    device: {
+        brand: 'hp',
+        model: 'X34',
+        releasedYear: '2015',
+    },
+    smartWatch: {
+        heartRate: "200",
+        callSupport: true,
+        calculator: true,
+        AiFeature: true,
+    },
+    bike: null
+
+}
